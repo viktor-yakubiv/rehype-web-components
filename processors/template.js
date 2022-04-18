ef60f142e64f571c653cb89ff76553308f525df1
Viktor Yakubiv <viktor@yakubiv.com>
2022-04-18 16:18:55 +0200

    Use rehype-lodash-template instead own values 💥
    Breaks the default syntax and so far does not allow to configure it.
    
    In general, it enables scripting similar to default template literal
    in JavaScript.

--- processors/template.js
+++ processors/template.js
@@ -1,4 +1,5 @@
 import clone from 'lodash.clonedeep'
+import substituteValues from 'rehype-lodash-template'
 import { unified } from 'unified'
 import { visit } from 'unist-util-visit'
 
@@ -48,73 +49,6 @@ const hasValue = node => {
   return valueTest.test(text)
 }
 
-// TODO: Consider using lodash.template instead
-//       It has some downsides, it's basically an overkill
-const substituteValues = values => shadowTree => {
-  const usedValues = new Set()
-
-  const substitude = text => {
-    const placeholders = text.match(new RegExp(valueTest, 'g')) ?? []
-
-    const substitutions = placeholders.map(occurance => {
-      const { name } = occurance.match(valueTest).groups
-      usedValues.add(name)
-
-      const value = Object.hasOwn(values, name)
-        ? values[name]
-        : occurance
-
-      return [occurance, value]
-    })
-
-    // Handles returning null when thee property is set purely to match another
-    if (substitutions.length === 1 && text === substitutions[0][0]) {
-      return substitutions[0][1]
-    }
-
-    return substitutions.reduce(
-      (str, [occurance, replacement]) =>
-        str.replaceAll(occurance, replacement ?? ''),
-      text,
-    )
-
-    return result
-  }
-
-  visit(shadowTree, hasValue, node => {
-    if (node.properties != null) {
-      const processedPropEntries = Object.entries(node.properties)
-        .map(([propName, propValue]) => {
-          // className and some others
-          if (Array.isArray(propValue)) {
-            return [
-              propName,
-              propValue.map(substitude).filter(newValue => newValue != null)]
-          }
-
-          return [propName, substitude(propValue)]
-        })
-        .filter(([propName, newValue]) => newValue != null)
-
-      node.properties = Object.fromEntries(processedPropEntries)
-    }
-
-    if (node.value != null) {
-      node.value = substitude(node.value) ?? ''
-    }
-  })
-
-  if (shadowTree.children.length === 1) {
-    const passProps = Object.fromEntries(
-      Object.entries(values)
-      .filter(([name, value]) => !usedValues.has(name) && value != null)
-    )
-
-    const shadowChild = shadowTree.children[0]
-    shadowChild.properties = Object.assign(shadowChild.properties ?? {}, passProps)
-  }
-}
-
 
 /**
  * API class
@@ -138,7 +72,7 @@ class Template {
 
     return unified()
       .use(substituteSlots, slots)
-      .use(substituteValues, properties)
+      .use(substituteValues, { values: properties })
       .use(appendPosition, hostTree.position)
       .run(shadowTree)
   }
